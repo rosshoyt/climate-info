64d293ef009d29e604f7531e8cb10c411bf16bbe
roshoyt <rosshoytmusic@gmail.com>
2021-07-01 19:54:26 -0700

    Cleaned up App.js, added material-ui location selector

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LocationSelect from './Components/LocationSelect';
+import { Grid, Button } from '@material-ui/core';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
   const [averageTemp, setAverageTemp] = useState(0);
 
-  useEffect(() => {
-    fetch('/api/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  },[]);
-  
   useEffect(() => {
     fetch('/api/temperature').then(res => res.json()).then(data => {
       setAverageTemp(data.temperature);
     });
-  },[]);
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p><code>Climate Change Tools</code></p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>The current time is {currentTime}.</p>
-        <p>The average temperature for Seattle, WA in June 2020 was {averageTemp}°F.</p>
-      </header>
+      <h1><code>Climate Change Tools</code></h1>
+      <h3>The average temperature for Seattle, WA in June 2020 was {averageTemp}°F.</h3>
+      <Grid container direction="row" justify="center" alignItems="stretch">
+        <LocationSelect />
+        <Button variant="contained" color="primary">
+          Start
+        </Button>
+      </Grid>
     </div>
   );
 }
