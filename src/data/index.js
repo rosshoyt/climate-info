dd3ed0b2d867649de824f0bd9c74599de275ef9c
roshoyt <rosshoytmusic@gmail.com>
2021-07-05 13:21:35 -0700

    Installed Nivo Calendar chart, added example

--- src/data/index.js
+++ src/data/index.js
@@ -4,5 +4,6 @@ import radar from "./radar";
 import stream from "./stream";
 import pie from "./pie";
 import bump from "./bump";
+import calendar from "./calendar";
 
-export { bar, line, radar, stream, pie, bump };
+export { bar, line, radar, stream, pie, bump, calendar };
