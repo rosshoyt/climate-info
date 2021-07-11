0f8ba60aac5a08b9fffd8540c6dfcff3d8b2adef
roshoyt <rosshoytmusic@gmail.com>
2021-07-10 23:35:02 -0700

    Add GSOMVisualization component to app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import MaxTempVisualization from './Components/MaxTempVisualization';
+import GSOMVisualization from './Components/GSOMVisualization';
 
 function App() {
-  const [averageTemp, setAverageTemp] = useState(0);
-
-  useEffect(() => {
-    fetch('/api/gsom/TAVG/CITY:US530018/2021-06-01/2021-06-30').then(res => res.json()).then(data => {
-      setAverageTemp(data.temperature);
-    });
-  }, []);
 
   return (
     <div className="App">
       <h1><code>Climate Change Tools</code></h1>
-      <h3>The average temperature for Seattle, WA in June 2021 was {averageTemp}°F.</h3>
-      <MaxTempVisualization/ >
+      <MaxTempVisualization/>
+      <GSOMVisualization/>
     </div>
   );
 }
