bc420b86e2763fb15dd5e5d030c5d429926e29cf
roshoyt <rosshoytmusic@gmail.com>
2021-07-11 16:22:19 -0700

    Implement light/dark mode toggle

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import MaxTempVisualization from './Components/MaxTempVisualization';
 import GSOMVisualization from './Components/GSOMVisualization';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { Switch, Typography, Paper } from '@material-ui/core';
+
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    }
+  });
 
   return (
-    <div className="App">
-      <h1><code>Climate Change Tools</code></h1>
-      <MaxTempVisualization/>
-      <GSOMVisualization/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: "100vh" }}>
+        <div className="App">
+          <Typography variant="h1"><code>Climate Change Tools</code></Typography>
+          <MaxTempVisualization />
+          <GSOMVisualization />
+          <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
