aa5376ccd50cda05c3990901470bfebf03a94a63
roshoyt <rosshoytmusic@gmail.com>
2021-07-11 19:12:14 -0700

    Improve layout of visualization components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
-import { Paper } from '@material-ui/core';
+import { Box, Grid, Container, Paper } from '@material-ui/core';
 import TitleAppBar from './Components/TitleAppBar';
 import MaxTempVisualization from './Components/MaxTempVisualization';
 import GSOMVisualization from './Components/GSOMVisualization';
@@ -12,17 +12,29 @@ function App() {
   const theme = createTheme({
     palette: {
       type: darkMode ? "dark" : "light",
-    }
+    },
+    paper: {
+      textAlign: 'left'
+    },
   });
 
   return (
     <ThemeProvider theme={theme}>
       <Paper style={{ height: "100vh" }}>
         <div className="App">
-          <TitleAppBar title="ClimateInfo.us" darkMode={darkMode} setDarkMode={setDarkMode}/>
-          <MaxTempVisualization/>
-          <GSOMVisualization/>   
+          <TitleAppBar title="ClimateInfo.US" darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Container maxWidth="xl">
+            <Grid container direction="column">
+              <Box m={5} >
+                <MaxTempVisualization />
+              </Box>
+              <Box m={5} >
+                <GSOMVisualization />
+              </Box>
+            </Grid>
+          </Container>
         </div>
+
       </Paper>
     </ThemeProvider>
   );
