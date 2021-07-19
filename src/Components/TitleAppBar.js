56220d4d214a4eac64418b8b120ee8e89b0e5fe7
roshoyt <rosshoytmusic@gmail.com>
2021-07-19 16:53:03 -0700

    Added labels to dark-mode switch

--- src/Components/TitleAppBar.js
+++ src/Components/TitleAppBar.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Switch from '@material-ui/core/Switch'
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function TitleAppBar({title, darkMode, setDarkMode}) {
+export default function TitleAppBar({ title, darkMode, setDarkMode }) {
   const classes = useStyles();
 
   return (
@@ -32,7 +33,15 @@ export default function TitleAppBar({title, darkMode, setDarkMode}) {
           <Typography variant="h4" className={classes.title} align='left'>
             {title}
           </Typography>
-          <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode) } />
+          <Typography component="div">
+            <Grid component="label" container alignItems="center" spacing={1}>
+              <Grid item>light</Grid>
+              <Grid item>
+                <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} name="checkedC" />
+              </Grid>
+              <Grid item>dark</Grid>
+            </Grid>
+          </Typography>
         </Toolbar>
       </AppBar>
     </div>
