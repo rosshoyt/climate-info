22f83ca1e6a0b020c86e93f21bbe2c9627bfdc16
roshoyt <rosshoytmusic@gmail.com>
2021-11-04 16:12:22 -0700

    Add Grid, YearPickers, Button

--- src/Components/GSOMVisualization.js
+++ src/Components/GSOMVisualization.js
@@ -1,42 +1,56 @@
 import React, { useEffect, useState } from 'react';
+import { Grid, Button } from '@material-ui/core';
 import LocationSelect from './LocationSelect';
 import VisualizationTitle from './VisualizationTitle';
+import YearPicker from './YearPicker';
 
 const GSOMVisualization = () => {
-
-  useEffect(() => {
-    updateData();
-  }, []);
-
   const [dataType, setDataType] = useState('TAVG');
 
   const [location, setLocation] = useState('CITY:US530018');
 
-  const [startDate, setStartDate] = useState('2021-06-01');
+  // const [startDate, setStartDate] = useState('2021-06-01');
 
-  const [endDate, setEndDate] = useState('2021-06-02');
+  // const [endDate, setEndDate] = useState('2021-06-02');
+
+  // TODO create year/date selector component
+  const [year1, setYear1] = useState(2021);
+  const [year2, setYear2] = useState(1991);
+  
+  const[refreshChartData, setRefreshChartData] = useState(false);
 
   // An example statistic from the GSOM api
   const [averageTemp, setAverageTemp] = useState(0);
 
   function updateData() {
-    console.log('GSOM Visualization fetching data...')
-    let url = '/api/gsom/' + dataType + '/' + location + '/' + startDate + '/' + endDate;
-    console.log('Updating data, fetching ' + url);
-    fetch(url).then(res => res.json()).then(recData => {
-      // set the example value for now  TODO update a graph or other visualization with GSOM data
-      setAverageTemp(recData.temperature);
-    });
+    console.log('GSOM Visualization fetching data... year1/2 = ', year1, year2)
+    // let url = '/api/gsom/' + dataType + '/' + location + '/' + year1 + '/' + endDate;
+    // console.log('Updating data, fetching ' + url);
+    // fetch(url).then(res => res.json()).then(recData => {
+    //   // set the example value for now  TODO update a graph or other visualization with GSOM data
+    //   setAverageTemp(recData.temperature);
+    // });
   }
+
+  
+  useEffect(() => {
+    updateData();
+  }, [refreshChartData]);
 
 
 
   return (
     <>
-      <VisualizationTitle title="Global Summary of the Month API"/>
-
+      <Grid container direction="column" justifyContent="space-between" alignItems="center">
+        <VisualizationTitle title="Global Summary of the Month API"/>
         <LocationSelect setLocation={setLocation} />
-        
+        <YearPicker label="Year 1" initialYear={year1} handleDateChange={setYear1} />
+        <YearPicker label="Year 2" initialYear={year2} handleDateChange={setYear2} />
+        <Button variant="contained" color="primary" size="large" onClick={() => setRefreshChartData(!refreshChartData)}>
+            Update Chart
+          </Button>
+      </Grid>
+
 
       {/*<h3>[Placeholder API usage]:The average temperature for Seattle, WA in June 2021 was {averageTemp}°F.</h3>
        <Grid container direction="row" justify="center" alignItems="stretch">
