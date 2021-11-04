61577a69a295e173679ea6222a1a56efac780b6d
roshoyt <rosshoytmusic@gmail.com>
2021-11-04 16:11:51 -0700

    Add state to YearPicker

--- src/Components/YearPicker.js
+++ src/Components/YearPicker.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { DatePicker } from "@material-ui/pickers";
 
-function YearPicker({label, handleDateChange}) {
+function YearPicker({label="", initialYear=2021, handleDateChange}) {
+  const [selectedDate, selectedDateChanged] = useState(new Date("01/01/2021"));
 
+  useEffect(() => {
+    selectedDateChanged(new Date("01/01/" + initialYear));
+  }, []);
+
+  function onChange(newDate) {
+    console.log('In YearPicker.onChange, new date:', newDate.year());
+    selectedDateChanged(newDate);
+    handleDateChange(newDate.year());
+  }
   return (
       <DatePicker
         views={["year"]}
-        label={"Year"}
-        value="1992"
-        onChange={handleDateChange}
+        label={label}
+        value={selectedDate}
+        onChange={onChange}
       />
   );
 }
