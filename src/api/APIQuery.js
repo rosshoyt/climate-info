92ed81529e456fa17d7d08d07404db48bbc322a7
roshoyt <rosshoytmusic@gmail.com>
2021-11-06 14:07:50 -0700

    Fix export and variable errors

--- src/api/APIQuery.js
+++ src/api/APIQuery.js
@@ -1,9 +1,7 @@
-class APIQuery {
+export default class APIQuery {
     // TODO set URL values elsewhere, store passed parameters as object fields
     constructor(location_id, start_date, end_date){
         this.url = "https://www.ncdc.noaa.gov/cdo-web/api/v2"
-        // First, we'll setup the request URL
-        this.url = URL_NOAA_API
         // Get average monthly temperature for location during time range
         this.url += "/data?"
         this.url += "datasetid=GHCND"
@@ -17,4 +15,4 @@ class APIQuery {
         this.url += "&enddate=" + end_date
         this.url += "&limit=500"
     }
-} 
+}
