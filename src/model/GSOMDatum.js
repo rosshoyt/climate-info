692dc2a712ed337d136faa0ae67aa6999b21cfed
roshoyt <rosshoytmusic@gmail.com>
2021-11-05 21:38:00 -0700

    Create GSOMDatum.js

---
+++ src/model/GSOMDatum.js
@@ -0,0 +1,7 @@
+function GSOMDatum(attribute, datatype, date, station, value) {
+    this.attribute = attribute;
+    this.datatype = datatype;
+    this.date = date;
+    this.station = station;
+    this.value = value;
+}
