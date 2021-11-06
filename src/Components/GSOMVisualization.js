20c033499e5be8a5dcfaeff0c5d86d52bcc4a37f
roshoyt <rosshoytmusic@gmail.com>
2021-11-06 12:52:11 -0700

    Add ScatterPlot chart, started formatting data for chart

--- src/Components/GSOMVisualization.js
+++ src/Components/GSOMVisualization.js
@@ -3,15 +3,14 @@ import { Grid, Button } from '@material-ui/core';
 import LocationSelect from './LocationSelect';
 import VisualizationTitle from './VisualizationTitle';
 import YearPicker from './YearPicker';
+import ScatterPlotChart from './Charts/ScatterPlotChart';
 
 const GSOMVisualization = () => {
   const [dataType, setDataType] = useState('TAVG');
 
   const [location, setLocation] = useState('CITY:US530018');
 
-  // const [startDate, setStartDate] = useState('2021-06-01');
-
-  // const [endDate, setEndDate] = useState('2021-06-02');
+  const [chartData, setChartData] = useState()
 
   // TODO create year/date selector component
   const [year1, setYear1] = useState(2021);
@@ -19,25 +18,46 @@ const GSOMVisualization = () => {
   
   const[refreshChartData, setRefreshChartData] = useState(false);
 
-  // An example statistic from the GSOM api
-  const [averageTemp, setAverageTemp] = useState(0);
-
   function updateData() {
-    console.log('GSOM Visualization fetching data... year1/2 = ', year1, year2)
+    // Clear chart data
+    setChartData({})
+    // TODO show loading icon
+    
+    // Fetch Data TODO fetch for each selected year in UI, and each month of each year instead of just January
+    console.log('GSOM Visualization fetching data... year1/2 = ', year1, year2);
     const url = '/api/gsom/' + dataType + '/' + location + '/' + year1 +'-01-01/' + year1 + '-12-31';
     console.log('Updating data, fetching ' + url);
+     
     fetch(url).then(res => res.json()).then(recData => {
-      console.log(recData);
+      // TODO if more queries added, maybe only do once all additional data is collected
+      // Or, could add data to chart as it comes in
+      processData(recData); 
     });
   }
 
-  
+  function processData(data) {
+    const results = data['results'];
+
+    results.forEach(r => {
+      console.log(r);
+    });
+
+    // expected format:
+    // {
+    //   "id": "group A",
+    //   "data": [
+    //     {
+    //       "x": 15,
+    //       "y": 81
+    //     },
+  }
+
   useEffect(() => {
     updateData();
   }, [refreshChartData]);
 
 
-
+  
   return (
     <>
       <Grid container direction="column" justifyContent="space-between" alignItems="center">
@@ -47,24 +67,13 @@ const GSOMVisualization = () => {
         <YearPicker label="Year 2" initialYear={year2} handleDateChange={setYear2} />
         <Button variant="contained" color="primary" size="large" onClick={() => setRefreshChartData(!refreshChartData)}>
             Update Chart
-          </Button>
-      </Grid>
-
-
-      {/*<h3>[Placeholder API usage]:The average temperature for Seattle, WA in June 2021 was {averageTemp}°F.</h3>
-       <Grid container direction="row" justify="center" alignItems="stretch">
-        <LocationSelect setLocation={setLocation} />
-        <DatePicker label='Start' defaultValue={startDate} setDate={setStartDate} />
-        <DatePicker label='End' defaultValue={endDate} setDate={setEndDate} />
-        <Button onClick={updateChart} variant="contained" color="primary">
-          Start
         </Button>
-      </Grid> 
-      <div style={{ height: 500 }}>
-        <LineChart data={chartData} />
-      </div>*/}
+        <div style={{ height: 500, width: 700 }}>
+          <ScatterPlotChart />
+        </div>
+      </Grid>
     </>
   );
 }
 
-export default GSOMVisualization;
+export default GSOMVisualization; 
