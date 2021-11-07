e6cd8d3afb820af8f70b84119e4047c4b9fd188e
roshoyt <rosshoytmusic@gmail.com>
2021-11-06 18:32:51 -0700

    Simplify UI layout

--- src/Components/MaxTempVisualization.js
+++ src/Components/MaxTempVisualization.js
@@ -76,56 +76,31 @@ const MaxTempVisualization = () => {
       return formattedDataList;
     }, []);
   }
-  // // formats a request's data for Nivo line chart
-  // function processData(data) {
-  //   let list = [];
-  //   for (const [key, value] of Object.entries(data)) {
-  //     // x value is assumed to be a timestamp
-  //     let entry = { x: moment(key).format('M-D'), y: value };
-  //     list.push(entry);
-  //   }
-  //   return list;
-  // }
 
   return (
     <>
-     <Grid container direction="row" justifyContent="space-between" alignItems="left">
-        <Grid item xs={10}>
-          <Typography noWrap variant='h4' align='left' fontWeight="fontWeightBold">
-            Peak Daily Temperature in {location} from {startDate} to {endDate}</Typography>
-        </Grid>
-        <Grid item xs={2}>
-          <Button variant="contained" color="primary" size="large" onClick={() => setRefreshChartData(!refreshChartData)}>
-            Update Chart
-          </Button>
-        </Grid>
-
-        <Grid item xs={12} align="left">
-          <LocationSelect setLocation={setLocation} />
-        </Grid>
-        
-        <Grid item xs={2}>
-          <Typography variant="h5">Main Time Period:</Typography>
-        </Grid>
-        <Grid item xs={2}>
-          <DatePicker label='Start' defaultValue={startDate} setDate={setStartDate} />
-        </Grid>
-        <Grid item xs={2}>
-          <DatePicker label='End' defaultValue={endDate} setDate={setEndDate} />
-        </Grid>
-
-        <Grid item xs={2}>
-          <Typography noWrap variant="h5">Compare to year:</Typography>
-        </Grid>
-        <Grid item xs={2}>
-          <YearPicker handleDateChange={setOtherYear} />
-        </Grid>
+      <Grid container direction="column" justifyContent="space-between" alignItems="center">
+        <Typography noWrap variant='h4' align='left' fontWeight="fontWeightBold">
+          Climate Data Explorer
+        </Typography>
+        <LocationSelect setLocation={setLocation} />
+        <Typography variant="h5">Main Time Period:</Typography>
+        <DatePicker label='Start' defaultValue={startDate} setDate={setStartDate} />
+        <DatePicker label='End' defaultValue={endDate} setDate={setEndDate} />
+        <Typography noWrap variant="h5">Compare to year:</Typography>
+        <YearPicker handleDateChange={setOtherYear} />
+        <Button 
+          variant="contained" 
+          color="primary" 
+          size="large" 
+          onClick={() => setRefreshChartData(!refreshChartData)}
+        >
+          Update Chart
+        </Button>
+        <Typography noWrap variant='h5' align='left' fontWeight="fontWeightBold">
+          Peak Daily Temperature in {location} from {startDate} to {endDate}:
+        </Typography>
       </Grid>
-
-
-      <Grid container direction="row" justify="left" alignItems="stretch">
-      </Grid>
-
       <div style={{ height: 500 }}>
         <ScatterPlotChart data={chartData} />
       </div>
