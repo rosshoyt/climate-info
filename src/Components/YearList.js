d5f16098601839d00964987a16fc7b8b1ead07c5
roshoyt <rosshoytmusic@gmail.com>
2021-11-07 11:04:46 -0800

    Add button icon, fix code indentation

--- src/Components/YearList.js
+++ src/Components/YearList.js
@@ -1,25 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import useStore from "../store";
 import { Button } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 import DatePicker from './DatePicker';
 
-
 const YearListItems= () => {
-    const store = useStore(state => state);
+  const store = useStore(state => state);
 
-    return (
-        <>
-            {store.years.map(year => (
-                 <DatePicker
-                 views={["year"]}
-                 // label={label}
-                 value={year.year}
-                 onChange={(date) => store.updateYear(year.id, date)}
-               />
-            ))}
-            <Button onClick={(evt) => store.addYear()}>Add Year</Button>
-        </>
-    );
+  return (
+    <>
+      {store.years.map(year => (
+        <DatePicker
+        views={["year"]}
+        // label={label}
+        value={year.year}
+        onChange={(date) => store.updateYear(year.id, date)}
+        />
+      ))}
+      <Button 
+        varient="outlined" 
+        color="primary"
+        onClick={(evt) => store.addYear()}
+        >
+        <AddIcon />Add Year
+      </Button>            
+    </>
+  );
 }
 
 function YearList() {
