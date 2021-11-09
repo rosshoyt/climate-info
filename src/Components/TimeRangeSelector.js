a045806e196789cc78d16f5bb9b17e8c3ad25cfe
roshoyt <rosshoytmusic@gmail.com>
2021-11-09 01:02:39 -0800

    Fill TimeRange selector with default day data

--- src/Components/TimeRangeSelector.js
+++ src/Components/TimeRangeSelector.js
@@ -1,56 +1,70 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from "moment";
 import { ResponsiveTimeRange } from '@nivo/calendar';
 import defaultData from '../Data/nivo-default-data/timerange';
+import { CircularProgress } from '@material-ui/core';
 // website examples showcase many properties,
 // you'll often use just a few of them.
-const TimeRangeSelector = ({ data=defaultData /* see data tab */ }) => {
-  
-  function getTimerangeData() {
-    let array = [];
-    const currentMoment = moment('2017-01-01', 'YYYY-MM-DD');
-    const endMoment = moment('2018-01-01', 'YYYY-MM-DD');
-    while (currentMoment.isBefore(endMoment, 'day')) {
-      //console.log(`Loop at ${currentMoment.format('YYYY-MM-DD')}`);
-      array.push({
-        "value": 1,
-        "day": currentMoment.format('YYYY-MM-DD')
-      })
-      // {
-    //   "value": 200,
-    //   "day": "2018-07-09"
-    // },
-      currentMoment.add(0, 'days');
-    }
-    return array;
+function getTimerangeData() {
+  console.log('getting timerange data')
+  let array = [];
+  const currentMoment = moment('2017-01-01', 'YYYY-MM-DD');
+  const endMoment = moment('2018-01-01', 'YYYY-MM-DD');
+  while (currentMoment.isBefore(endMoment, 'day')) {
+    //console.log(`Loop at ${currentMoment.format('YYYY-MM-DD')}`);
+    array.push({
+      "value": 0,
+      "day": currentMoment.format('YYYY-MM-DD')
+    })
+    // {
+  //   "value": 200,
+  //   "day": "2018-07-09"
+  // },
+    currentMoment.add(1, 'days');
   }
+  console.log(array);
+  return array;
+}
+
+const TimeRangeSelector = ({ data=getTimerangeData() /* see data tab */ }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  
+  useEffect(() => {
+  }, [])
+
+ 
 
   return (
-    <ResponsiveTimeRange
-      data={data}
-      from="2018-01-01"
-      to="2018-12-31"
-      emptyColor="#ededed"
-      colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
-      //margin={{ top: 40, right: 40, bottom: 100, left: 40 }}
-      dayBorderWidth={2}
-      dayBorderColor="#000000"
-      // legends={[
-      //     {
-      //         anchor: 'bottom-right',
-      //         direction: 'row',
-      //         justify: false,
-      //         itemCount: 4,
-      //         itemWidth: 42,
-      //         itemHeight: 36,
-      //         itemsSpacing: 14,
-      //         itemDirection: 'right-to-left',
-      //         translateX: -60,
-      //         translateY: -60,
-      //         symbolSize: 20
-      //     }
-      // ]}
-    />
+     <>
+      { isLoading ? (
+        <CircularProgress />) : (
+        <ResponsiveTimeRange
+          data={data}
+          from="2018-01-01"
+          to="2018-12-31"
+          emptyColor="#ededed"
+          colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
+          //margin={{ top: 40, right: 40, bottom: 100, left: 40 }}
+          dayBorderWidth={2}
+          dayBorderColor="#000000"
+          // legends={[
+          //     {
+          //         anchor: 'bottom-right',
+          //         direction: 'row',
+          //         justify: false,
+          //         itemCount: 4,
+          //         itemWidth: 42,
+          //         itemHeight: 36,
+          //         itemsSpacing: 14,
+          //         itemDirection: 'right-to-left',
+          //         translateX: -60,
+          //         translateY: -60,
+          //         symbolSize: 20
+          //     }
+          // ]}
+        />
+    ) }
+    </>
   );
 }
 
