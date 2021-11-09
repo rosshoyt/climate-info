8f4e7b9cbc184f5a001e497ffb6278d76e4a4ec0
roshoyt <rosshoytmusic@gmail.com>
2021-11-09 13:56:54 -0800

    Create TimeRangeChart.js,  use in TimeRangeSelector

---
+++ src/Components/Charts/TimeRangeChart.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ResponsiveTimeRange } from '@nivo/calendar';
+const TimeRangeChart  = ({ data, handleDateClicked }) => {
+
+  return (
+    <ResponsiveTimeRange
+      data={data}
+      from="2018-01-01"
+      to="2018-12-31"
+      domain={[1,150]}
+      colors={[ '#61cdbb', '#ededed']}//'#97e3d5', '#e8c1a0', '#f47560' ]}
+      //colors={[ '#61cdbb','#97e3d5', '#e8c1a0', '#f47560' ]}
+      //margin={{ top: 40, right: 40, bottom: 100, left: 40 }}
+      dayBorderWidth={2}
+      dayBorderColor="#000000"
+      daySpacing={4}
+      onClick={(day,event)=>handleDateClicked(day, event)}
+      // legends={[
+      //     {
+      //         anchor: 'bottom-right',
+      //         direction: 'row',
+      //         justify: false,
+      //         itemCount: 4,
+      //         itemWidth: 42,
+      //         itemHeight: 36,
+      //         itemsSpacing: 14,
+      //         itemDirection: 'right-to-left',
+      //         translateX: -60,
+      //         translateY: -60,
+      //         symbolSize: 20
+      //     }
+      // ]}
+    />
+  );
+}
+
+export default TimeRangeChart;
--- src/Components/TimeRangeSelector.js
+++ src/Components/TimeRangeSelector.js
@@ -1,68 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import moment from "moment";
+
 import { ResponsiveTimeRange } from '@nivo/calendar';
-import defaultData from '../Data/nivo-default-data/timerange';
 import { CircularProgress } from '@material-ui/core';
-// website examples showcase many properties,
-// you'll often use just a few of them.
-function getTimerangeData() {
+import TimeRangeChart from './Charts/TimeRangeChart'
+import moment from "moment";
+function getTimeRangeData() {
   console.log('getting timerange data')
   let array = [];
   const currentMoment = moment('2017-01-01', 'YYYY-MM-DD');
   const endMoment = moment('2018-01-01', 'YYYY-MM-DD');
+  let currVal = 0
   while (currentMoment.isBefore(endMoment, 'day')) {
     //console.log(`Loop at ${currentMoment.format('YYYY-MM-DD')}`);
     array.push({
       "value": 0,
       "day": currentMoment.format('YYYY-MM-DD')
     })
-    // {
-  //   "value": 200,
-  //   "day": "2018-07-09"
-  // },
     currentMoment.add(1, 'days');
+    currVal++;
   }
   console.log(array);
   return array;
 }
 
-const TimeRangeSelector = ({ data=getTimerangeData() /* see data tab */ }) => {
+const TimeRangeSelector = () => {
   const [isLoading, setIsLoading] = useState(false);
   
+  const [timeRangeData, setTimeRangeData] = useState(getTimeRangeData());
+
   useEffect(() => {
   }, [])
 
  
+  // TODO get custom tooltip, only show Month/day of hover
+  function getTooltip(){
 
+  }
+  function dateClicked(day, event){
+    console.log('clicked', day, 'event', event);
+    
+    setTimeRangeData(timeRangeData.map(dayValPair => ({
+      ...dayValPair,
+      value: dayValPair.day === day.day ? 100 : dayValPair.value
+    })));
+    
+    console.log(timeRangeData)
+  }
   return (
      <>
       { isLoading ? (
         <CircularProgress />) : (
-        <ResponsiveTimeRange
-          data={data}
-          from="2018-01-01"
-          to="2018-12-31"
-          emptyColor="#ededed"
-          colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
-          //margin={{ top: 40, right: 40, bottom: 100, left: 40 }}
-          dayBorderWidth={2}
-          dayBorderColor="#000000"
-          // legends={[
-          //     {
-          //         anchor: 'bottom-right',
-          //         direction: 'row',
-          //         justify: false,
-          //         itemCount: 4,
-          //         itemWidth: 42,
-          //         itemHeight: 36,
-          //         itemsSpacing: 14,
-          //         itemDirection: 'right-to-left',
-          //         translateX: -60,
-          //         translateY: -60,
-          //         symbolSize: 20
-          //     }
-          // ]}
-        />
+          <TimeRangeChart data={timeRangeData} handleDateClicked={dateClicked}/>
     ) }
     </>
   );
