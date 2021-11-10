d63c07aab6b3798d6834e01386f619d71f7bdc8c
roshoyt <rosshoytmusic@gmail.com>
2021-11-09 16:03:55 -0800

    User can select and view timerange

--- src/Components/TimeRangeSelector.js
+++ src/Components/TimeRangeSelector.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-
-import { ResponsiveTimeRange } from '@nivo/calendar';
 import { CircularProgress } from '@material-ui/core';
 import TimeRangeChart from './Charts/TimeRangeChart'
 import moment from "moment";
-function getTimeRangeData() {
-  console.log('getting timerange data')
+
+
+// TODO figure out why this method being called more than at the start
+function getDefaultTimeRangeData() {
+  console.log('getting default timerange data')
   let array = [];
   const currentMoment = moment('2017-01-01', 'YYYY-MM-DD');
   const endMoment = moment('2018-01-01', 'YYYY-MM-DD');
@@ -19,32 +20,64 @@ function getTimeRangeData() {
     currentMoment.add(1, 'days');
     currVal++;
   }
-  console.log(array);
   return array;
 }
 
 const TimeRangeSelector = () => {
   const [isLoading, setIsLoading] = useState(false);
   
-  const [timeRangeData, setTimeRangeData] = useState(getTimeRangeData());
+  const [timeRangeData, setTimeRangeData] = useState(getDefaultTimeRangeData());
 
   useEffect(() => {
   }, [])
 
  
   // TODO get custom tooltip, only show Month/day of hover
-  function getTooltip(){
+  //function getTooltip(){}
 
+  const [isSelectingSecondDate, setIsSelectingSecondDate] = useState(false);
+
+  const [date1, setDate1] = useState(null);
+
+  function dateIsInRange(date, dateMin, dateMax){
+    let dateMoment = moment(date, 'YYYY-MM-DD');
+    console.log('Checking if is in range', dateMin, dateMax);
+    if(dateMoment.isSame(dateMin) || dateMoment.isSame(dateMax)) {
+      console.log('Date is same as a start or end date!');
+      return true;
+    } 
+    else if(dateMoment.isBetween(dateMin, dateMax)){
+      console.log('Date is in between start or end date!');
+      return true;
+    }
+    return false;
   }
+
   function dateClicked(day, event){
-    console.log('clicked', day, 'event', event);
-    
-    setTimeRangeData(timeRangeData.map(dayValPair => ({
-      ...dayValPair,
-      value: dayValPair.day === day.day ? 100 : dayValPair.value
-    })));
-    
-    console.log(timeRangeData)
+    if(isSelectingSecondDate){
+      console.log('Selected second date', day.day);
+
+      setTimeRangeData(timeRangeData.map(dayValPair => ({
+        ...dayValPair,
+        value: dateIsInRange(dayValPair.day, date1, day.day) ? 100 : dayValPair.value
+      })));
+      
+
+    } else {
+      // TODO clear any existing selections: setTimeRangeData(defData) doesn't work because of useState slow
+      // google await setState
+      //setTimeRangeData(getDefaultTimeRangeData());
+      
+      console.log('Selected first date');  
+      setDate1(day.day);
+      //console.log('Testing Date1 val', date1);
+      setTimeRangeData(timeRangeData.map(dayValPair => ({
+        ...dayValPair,
+        value: dayValPair.day === day.day ? 100 : dayValPair.value
+      })));
+
+    }
+    setIsSelectingSecondDate(!isSelectingSecondDate);
   }
   return (
      <>
