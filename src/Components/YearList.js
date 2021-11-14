d188e31b19f7dddad2776288e0740b2befccfa7b
roshoyt <rosshoytmusic@gmail.com>
2021-11-13 16:44:53 -0800

    Convert YearList to use MUI List

--- src/Components/YearList.js
+++ src/Components/YearList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import useStore from "../store";
-import { Button, Box } from '@material-ui/core';
+import { Button, Box, List, ListItem } from '@material-ui/core';
 import AddIcon  from '@material-ui/icons/Add';
 import EditIcon  from '@material-ui/icons/Edit';
 import { DatePicker } from "@material-ui/pickers";
@@ -10,47 +10,52 @@ const YearListItems= () => {
   const store = useStore(state => state);
   
   return (
-    <>
+    <List>
       {store.years.map(year => (
         <>
-        <Box display="flex" justifyContent="center" >
-          <DatePicker
-            views={['year']}
-            // label={label}
-            value={ new Date(year.year, 1, 1) } // fill in dummy month/day vals with JS date
-            onChange={(date) => store.updateYear(year.id, date.year())} // TODO 'date' here is a moment object, for some reason...
-          />
-          <Box
-            onClick= {(event)=>store.updateYearColorSelectorOpen(year.id, !year.colorSelectorOpen)}
-            sx={{
-              width: 100,
-              bgcolor: year.color,
-            }}
-          >
-            <EditIcon/>{year.color}
-          </Box>
-          
-        </Box>
-        { year.colorSelectorOpen ? (
-          <TwitterPicker onChangeComplete={(c) => store.updateYearColor(year.id, c.hex)}/>
-        ) : <></>}
-      </>
+          <ListItem>
+            <Box display="flex" justifyContent="center" >
+              <DatePicker
+                views={['year']}
+                // label={label}
+                value={ new Date(year.year, 1, 1) } // fill in dummy month/day vals with JS date
+                onChange={(date) => store.updateYear(year.id, date.year())} // TODO 'date' here is a moment object, for some reason...
+              />
+              <Box
+                onClick= {(event)=>store.updateYearColorSelectorOpen(year.id, !year.colorSelectorOpen)}
+                sx={{
+                  width: 100,
+                  bgcolor: year.color,
+                }}
+              >
+                <EditIcon/>{year.color}
+              </Box>
+              
+            </Box>
+          </ListItem>
+          { year.colorSelectorOpen ? (
+            <ListItem>
+              <TwitterPicker onChangeComplete={(c) => store.updateYearColor(year.id, c.hex)}/>
+            </ListItem>
+          ) : <></>}
+        </>
       ))}
-      <Button 
-        varient="outlined" 
-        color="primary"
-        onClick={(evt) => store.addYear()}
-        >
-        <AddIcon />Add Year
-      </Button>            
-    </>
+      <ListItem>
+        <Button 
+          varient="outlined" 
+          color="primary"
+          onClick={(evt) => store.addYear()}
+          >
+          <AddIcon />Add Year
+        </Button>
+      </ListItem>
+    </List>
   );
 }
 
 function YearList() {
   return (
     <>
-      <div>Year List</div>
       <YearListItems />
     </>
   );
