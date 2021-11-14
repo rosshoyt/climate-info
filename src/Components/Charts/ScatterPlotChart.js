ab2a9a8923e21fc7fb438f894db3fe6fd0a33e79
roshoyt <rosshoytmusic@gmail.com>
2021-11-14 01:17:50 -0800

    Add slider for graph line size, add line curve

--- src/Components/Charts/ScatterPlotChart.js
+++ src/Components/Charts/ScatterPlotChart.js
@@ -1,90 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ResponsiveScatterPlot } from '@nivo/scatterplot'
 import scatter from '../../Data/nivo-default-data/scatter';
 import useStore from '../../store';
-import { line } from 'd3-shape';
+import { line, curveNatural } from 'd3-shape';
+import { Box, Slider, Grid } from '@material-ui/core/';
 
 const ScatterPlotChart = ({ data=scatter/* see data tab */ }) => {
+
+    const [lineWidth, setLineWidth] = useState(10);
+
     const yearInfo = useStore(state => state.years);
     function getColor(year) {
         return yearInfo.find(y => y.year === year).color;
     }
     return (
-        <ResponsiveScatterPlot
-            data={data}
-            //colors={{ datum: 'data.color' }}
-            colors={(node) => {
-                return getColor(node.serieId);
-            }}
-            theme={{
-                fontSize: 16,
-            }}
-            margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
-            xScale={{ type: 'point', min: 0, max: 'auto' }}
-            xFormat=" >-.2f"
-            yScale={{ type: 'linear', min: 'auto', max: 'auto' }}
-            yFormat=">-.2f"
-            blendMode="multiply"
-            nodeSize={18}
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-                orient: 'bottom',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'Date',
-                legendPosition: 'middle',
-                legendOffset: 46
-            }}
-            axisLeft={{
-                orient: 'left',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'Temperature (F)',
-                legendPosition: 'middle',
-                legendOffset: -60
-            }}
-            legends={[
-                {
-                    anchor: 'bottom-right',
-                    direction: 'column',
-                    justify: false,
-                    translateX: 130,
-                    translateY: 0,
-                    itemWidth: 100,
-                    itemHeight: 12,
-                    itemsSpacing: 5,
-                    itemDirection: 'left-to-right',
-                    symbolSize: 20,
-                    symbolShape: 'circle',
-                    effects: [
-                        {
-                            on: 'hover',
-                            style: {
-                                itemOpacity: 1
+        <>
+            <ResponsiveScatterPlot
+                data={data}
+                //colors={{ datum: 'data.color' }}
+                colors={(node) => {
+                    return getColor(node.serieId);
+                }}
+                theme={{
+                    fontSize: 16,
+                }}
+                margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
+                xScale={{ type: 'point', min: 0, max: 'auto' }}
+                xFormat=" >-.2f"
+                yScale={{ type: 'linear', min: 'auto', max: 'auto' }}
+                yFormat=">-.2f"
+                blendMode="multiply"
+                nodeSize={18}
+                axisTop={null}
+                axisRight={null}
+                axisBottom={{
+                    orient: 'bottom',
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legend: 'Date',
+                    legendPosition: 'middle',
+                    legendOffset: 46
+                }}
+                axisLeft={{
+                    orient: 'left',
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legend: 'Temperature (F)',
+                    legendPosition: 'middle',
+                    legendOffset: -60
+                }}
+                legends={[
+                    {
+                        anchor: 'bottom-right',
+                        direction: 'column',
+                        justify: false,
+                        translateX: 130,
+                        translateY: 0,
+                        itemWidth: 100,
+                        itemHeight: 12,
+                        itemsSpacing: 5,
+                        itemDirection: 'left-to-right',
+                        symbolSize: 20,
+                        symbolShape: 'circle',
+                        effects: [
+                            {
+                                on: 'hover',
+                                style: {
+                                    itemOpacity: 1
+                                }
                             }
-                        }
-                    ]
-                }
-            ]}
-            layers={[
-                "grid",
-                "axes",
-                Line,
-                "nodes",
-                "markers",
-                "mesh",
-                "annotations",
-                "legends",
-              ]}
-        />
+                        ]
+                    }
+                ]}
+                layers={[
+                    "grid",
+                    "axes",
+                    Line,
+                    "nodes",
+                    "markers",
+                    "mesh",
+                    "annotations",
+                    "legends",
+                ]}
+            />
+            <Grid item>
+                <Box sx={{ width: 300 }}>
+                    <Slider
+                    key={`slider-${1}`}
+                        defaultValue={ lineWidth }
+                        onChange={ (e, newValue) => setLineWidth(newValue) }
+                        getAriaValueText={ (value) => `${value}` }
+                        aria-labelledby=" "
+                        step={5}
+                        marks
+                        min={0}
+                        max={50}
+                        valueLabelDisplay="auto"
+                    />
+                </Box>
+            </Grid>
+        </>
     );
 
     function Line({ data, nodes, xScale, yScale}) {
         // use the d3 line generator line()
-        const lineGenerator = line()
+        const lineGenerator = line().curve(curveNatural)
             .x(d => xScale(d.x))
             .y(d => yScale(d.y));
         
@@ -123,19 +145,11 @@ const ScatterPlotChart = ({ data=scatter/* see data tab */ }) => {
                 d={lineGenerator(data)}
                 fill="none"
                 stroke={getColor(id)}
-                // TODO improve style
-                style={
-                index % 2 === 0
-                    ? {
-                        // simulate line will dash stroke when index is even
-                        strokeDasharray: "3, 6",
-                        strokeWidth: 3
-                    }
-                    : {
-                        // simulate line with solid stroke
-                        strokeWidth: 3
-                    }
-                }
+                // TODO improve line style controls
+                style={{
+                        // strokeDasharray: "3, 6",
+                        strokeWidth: lineWidth
+                }}
             />);
         });
         
