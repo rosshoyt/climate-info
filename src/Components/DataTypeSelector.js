a32c1ed1e9e265cb5653e225183c6a9c5504413e
roshoyt <rosshoytmusic@gmail.com>
2021-11-18 15:32:12 -0800

    Create DataTypeSelector.js

---
+++ src/Components/DataTypeSelector.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-use-before-define */
+import React, { useState, useEffect } from 'react';
+import { makeStyles, FormControl, InputLabel, Select, MenuItem,FormHelperText } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function DataTypeSelector({setDataType}) {
+  const [dataTypes, setDataTypes] = useState( [ 
+    "TMAX",
+    "TMIN",
+    "PRCP",
+    "SNOW",
+    "SNWD",
+   ] );
+
+  const classes = useStyles();
+
+
+  const handleChange = (event) => {
+    setDataType(event.target.value);
+  };
+
+  return (
+    <FormControl className={classes.formControl}
+    >
+      <InputLabel id="demo-simple-select-label">DataType</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value="TMAX" //{dataTypes[0]} fix
+          onChange={handleChange}
+        >
+          {
+            dataTypes.map(dataType => {
+              return(
+                <MenuItem value={dataType}>{dataType}</MenuItem>
+              );
+            })
+          }
+        </Select>
+    </FormControl>
+  );
+}
