67e24646926f0554d57eda9c411a5fd418b4f3e6
roshoyt <rosshoytmusic@gmail.com>
2021-11-27 20:37:28 -0800

    Create ResponsiveListContainer.js

---
+++ src/Components/ResponsiveListContainer.js
@@ -0,0 +1,29 @@
+import React, { Children } from 'react'
+import useWindowDimensions from '../../Utils/WindowUtils';
+import CollapsibleList from './CollapsibleList';
+
+export default function ResponsiveListContainer({ children }) {
+  // const arrayChildren = Children.toArray(children)
+  const { height, width } = useWindowDimensions();
+
+  function isMobileBrowser() {
+    // TODO implement a shared function, perhaps in window utils
+    return true;
+  }
+  
+  // TODO implement horizontal container for desktop
+  return (
+    <>
+      {  isMobileBrowser()  ? (
+          <CollapsibleList>
+            { children } 
+          </CollapsibleList>
+        ) : (
+          <div>
+            { children }
+          </div>
+        )      
+      }
+    </>
+  );
+}
