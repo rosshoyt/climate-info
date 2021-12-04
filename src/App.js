1e3380829d6e91984200414f9816696772761333
roshoyt <rosshoytmusic@gmail.com>
2021-12-04 14:24:53 -0800

    Setup react-query with dev tools in App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
 // pick a date util library
 import MomentUtils from '@date-io/moment';
 import ClimateDataExplorer from './Components/Pages/ClimateDataExplorer';
 import FoodFootprintExplorer from './Components/Pages/FoodFootprintExplorer'
 import { blueGrey, orange } from '@material-ui/core/colors';
 import ResponsiveMiniDrawer from './Components/Drawers/ResponsiveMiniDrawer'
+
+ // React Query client
+ const queryClient = new QueryClient()
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -25,18 +30,20 @@ function App() {
   });
 
   return (
-    <MuiPickersUtilsProvider utils={MomentUtils}>
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <CssBaseline />
-          <ResponsiveMiniDrawer darkMode={darkMode} setDarkMode={setDarkMode}>
-            <ClimateDataExplorer path="/" name="Climate Data"/>
-            <FoodFootprintExplorer path="/food-footprint" name="Food Footprints"/>
-          </ResponsiveMiniDrawer>
-          
-        </div>
-      </ThemeProvider>
-    </MuiPickersUtilsProvider>
+    <QueryClientProvider client={queryClient}>
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <ThemeProvider theme={theme}>
+          <div className="App">
+            <CssBaseline />
+            <ResponsiveMiniDrawer darkMode={darkMode} setDarkMode={setDarkMode}>
+              <ClimateDataExplorer path="/" name="Climate Data"/>
+              <FoodFootprintExplorer path="/food-footprint" name="Food Footprints"/>
+            </ResponsiveMiniDrawer>
+          </div>
+        </ThemeProvider>
+      </MuiPickersUtilsProvider>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 }
 
