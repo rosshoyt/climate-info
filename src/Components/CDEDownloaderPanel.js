6a675007ea08cea94864e23cf4d7f4c178d44b7f
roshoyt <rosshoytmusic@gmail.com>
2021-12-13 15:30:57 -0800

    Create CDEDownloaderPanel.js

---
+++ src/Components/CDEDownloaderPanel.js
@@ -0,0 +1,21 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+
+export default function CDEDownloaderPanel() {
+  return (
+    <>
+      <div>
+        Chart:
+      </div>
+      <Button>
+        Download Chart (Coming soon!)
+      </Button>
+      <div>
+        Data:
+      </div>
+      <Button>
+        Download data (as CSV) (coming soon!)
+      </Button>
+    </>
+  )
+}
