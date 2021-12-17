2a7a2e2d4799bbb66bbb03ee02c88acf3fafde35
roshoyt <rosshoytmusic@gmail.com>
2021-12-17 12:19:23 -0800

    DataTable rerenders on change to raw data

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -4,20 +4,21 @@ import useStore from '../store'
 
 export default function DataTable() {
 
-  const timeseriesList = useStore(state => state.rawData);
+  const rawData = useStore(state => state.rawData);
   
   useEffect(() => {
-  }, []);
+  }, [rawData]);
 
   // reads the list of lists containing data and extracts keys from the first 
   // entry of the first list.
-  function updateColumns(timeseriesList) {
+  function updateColumns(rawData) {
     let newColumns = [];
-    if(timeseriesList.length > 0){
-      let timeseries = timeseriesList[0];
+    if(rawData.length > 0){
+      let timeseries = rawData[0];
       let data = timeseries['data']
       if(data.length > 0){
-        let dataEntry = data[0];        Object.keys(dataEntry).forEach(key => {
+        let dataEntry = data[0];        
+        Object.keys(dataEntry).forEach(key => {
           if(!newColumns.includes(key)) {
             newColumns.push(
               {
@@ -51,8 +52,8 @@ export default function DataTable() {
   return (
     <>
       <DataGrid
-        rows={ formatData(timeseriesList) }
-        columns={updateColumns(timeseriesList)}
+        rows={ formatData(rawData) }
+        columns={updateColumns(rawData)}
         pageSize={12}
         checkboxSelection
         disableSelectionOnClick
