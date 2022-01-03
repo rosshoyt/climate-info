181a07d69da04d72198a90024c840e93277beee3
roshoyt <rosshoytmusic@gmail.com>
2022-01-03 15:10:55 -0800

    Add list virtualization to handle large list size in CustomAutocomplete

--- src/Components/Autocomplete/CustomAutocomplete.js
+++ src/Components/Autocomplete/CustomAutocomplete.js
@@ -5,6 +5,12 @@ import CloseIcon from "@material-ui/icons/Close";
 import DoneIcon from "@material-ui/icons/Done";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import InputBase from "@material-ui/core/InputBase";
+import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import { useTheme } from '@material-ui/core/styles';
+import { VariableSizeList } from 'react-window';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -113,9 +119,95 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+
+const LISTBOX_PADDING = 0; // px
+
+function renderRow(props) {
+  const { data, index, style } = props;
+  return React.cloneElement(data[index], {
+    style: {
+      ...style,
+      top: style.top + LISTBOX_PADDING,
+    },
+  });
+}
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
+function useResetCache(data) {
+  const ref = React.useRef(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+// Adapter for react-window
+const ListboxComponent = React.forwardRef(function ListboxComponent(props, ref) {
+  const { children, ...other } = props;
+  const itemData = React.Children.toArray(children);
+  const theme = useTheme();
+  const smUp = useMediaQuery(theme.breakpoints.up('sm'), { noSsr: true });
+  const itemCount = itemData.length;
+  const itemSize = smUp ? 48 : 56;
+
+  const getChildSize = (child) => {
+    if (React.isValidElement(child) && child.type === ListSubheader) {
+      return 48;
+    }
+
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={(index) => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
+
+ListboxComponent.propTypes = {
+  children: PropTypes.node,
+};
+
+
 export default function CustomAutocomplete({ onClose, selectedOption, setSelectedOption, selectionOptions }) {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
+
+  React.useEffect(() => {
+    console.log('custom autocomplete, use effect')
+  }, [selectedOption, selectionOptions])
 
   const handleClose = (event, reason) => {
     console.log('in handle close');
@@ -150,6 +242,7 @@ export default function CustomAutocomplete({ onClose, selectedOption, setSelecte
             setSelectedOption(newValue);
             onClose();
           }}
+          ListboxComponent={ListboxComponent}
           disableCloseOnSelect
           disablePortal
           renderTags={() => null}
